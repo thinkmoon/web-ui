d29680ce13e1bc4f659045e876c1f6a55a14b94e
醉月思 <tim@thinkmoon.cn>
2022-04-10 21:15:05 +0800

    fix: 修复首页预览图

--- api/request/index.ts
+++ api/request/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-axios.defaults.baseURL = 'https://service.thinkmoon.cn/api';
-// axios.defaults.baseURL = 'http://127.0.0.1:9447/api';
+// axios.defaults.baseURL = 'https://service.thinkmoon.cn/api';
+axios.defaults.baseURL = 'http://127.0.0.1:9447/api';
 if (!process.server) {
   const auth = useCookie('auth')
   axios.defaults.headers.common['Authorization'] = auth.value;
