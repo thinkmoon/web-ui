63195ada8c9ebbba53cde2f8f3284a1c31144460
醉月思 <tim@thinkmoon.cn>
2022-04-18 22:00:48 +0800

    feat: 标签页增加标题

---
+++ pages/tag/[tid]/[pageIndex].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Title>标签""下的文章 | {{ config.TITLE }}</Title>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const config = useRuntimeConfig();
+
+</script>
+
+<style scoped>
+
+</style>
