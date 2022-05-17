7b67eb95ec26f7ffba33841c763560142dfd35fc
thinkmoon <thinkmoon@users.noreply.github.com>
2022-05-17 14:57:23 +0000

    deploy: c499c7500235457d73f67f7183d9f029f79dc722

---
+++ TagApi-4d880187.mjs
@@ -0,0 +1,1 @@
+import{E as a}from"./entry-2b853719.mjs";class s{static getTag(t){return a({method:"get",url:"/tag/list",params:t})}}export{s as T};
---
+++ public/nuxt-asset/auth-c79809f8.mjs
@@ -0,0 +1,1 @@
+import{L as a,G as t,H as e}from"./entry-2b853719.mjs";var s=a((o,u)=>{if(!t("auth").value)return e("/login")});export{s as default};
