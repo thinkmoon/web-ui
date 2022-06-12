a40dbb6a3e9a68529ebf725617a24d682bc7d476
醉月思 <tim@thinkmoon.cn>
2022-06-12 20:33:01 +0800

    fix: baseUrl问题

--- runtime.config.ts
+++ runtime.config.ts
@@ -4,5 +4,5 @@ export default {
   VERSION: `${process.env.npm_package_version} · build-${dayjs().format('YYYYMMDD')}`,
   KEYWORDS: ['thinkmoon', '指尖魔法屋', '醉月思的博客'],
   DESCRIPTION: 'web前端开发工程师、面向高保真编程、总结与记录是两个极其优秀的学习习惯、对知识和技术保持敬畏之心！',
-  baseUrl: process.env.NUXT_BASE_URL,
+  baseUrl: 'https://service.thinkmoon.cn/api',
 };
