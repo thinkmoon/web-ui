20f310e66388986c6752ea75789f51c996a5f9c6
醉月思 <tim@thinkmoon.cn>
2022-09-07 00:24:21 +0800

    cdnURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import runtimeConfig from './runtime.config';
 const lifecycle = process.env.npm_lifecycle_event;
 
 export default defineNuxtConfig({
-  cdnUrl: 'https://thinkmoon.github.io/thinkblog/_nuxt/public',
+  cdnURL: 'https://thinkmoon.github.io/thinkblog/_nuxt/public',
   publicRuntimeConfig: runtimeConfig,
   meta: {
     meta: [
@@ -33,7 +33,6 @@ export default defineNuxtConfig({
   },
   vite: {
     plugins: [
-      viteCompression()
     ]
   }
 });
