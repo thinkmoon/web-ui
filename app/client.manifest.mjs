7a2a75cb5cc34d376c0cfc8c83a1dacd01cff137
thinkmoon <thinkmoon@users.noreply.github.com>
2022-09-06 16:22:34 +0000

    deploy: f3ee5154da11d0bbb8fb43c2885f903c8ebc51b5

--- app/client.manifest.mjs
+++ app/client.manifest.mjs
@@ -368,15 +368,20 @@ const client_manifest = {
     "file": "editor.46f3a4ca.css",
     "src": "pages/admin/editor.css"
   },
+  "pages/category/index.css": {
+    "resourceType": "style",
+    "file": "index.c7621f5e.css",
+    "src": "pages/category/index.css"
+  },
   "pages/admin.css": {
     "resourceType": "style",
     "file": "admin.6e91f5a2.css",
     "src": "pages/admin.css"
   },
-  "pages/category/index.css": {
+  "pages/page/[pageIndex].css": {
     "resourceType": "style",
-    "file": "index.c7621f5e.css",
-    "src": "pages/category/index.css"
+    "file": "_pageIndex_.5f2a6f76.css",
+    "src": "pages/page/[pageIndex].css"
   },
   "pages/index.css": {
     "resourceType": "style",
@@ -388,35 +393,30 @@ const client_manifest = {
     "file": "login.2248ea52.css",
     "src": "pages/login.css"
   },
-  "pages/page/[pageIndex].css": {
+  "pages/tag/index.css": {
     "resourceType": "style",
-    "file": "_pageIndex_.5f2a6f76.css",
-    "src": "pages/page/[pageIndex].css"
+    "file": "index.506c05f8.css",
+    "src": "pages/tag/index.css"
   },
   "pages/post/[cid].css": {
     "resourceType": "style",
     "file": "_cid_.d85db5b7.css",
     "src": "pages/post/[cid].css"
   },
-  "pages/tag/index.css": {
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
     "resourceType": "style",
-    "file": "index.506c05f8.css",
-    "src": "pages/tag/index.css"
-  },
-  "layouts/default.css": {
-    "resourceType": "style",
-    "file": "default.e87c0fd5.css",
-    "src": "layouts/default.css"
+    "file": "error-500.08851880.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
   },
   "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
     "resourceType": "style",
     "file": "error-404.7729cee9.css",
     "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
   },
-  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+  "layouts/default.css": {
     "resourceType": "style",
-    "file": "error-500.08851880.css",
-    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+    "file": "default.e87c0fd5.css",
+    "src": "layouts/default.css"
   },
   "node_modules/nuxt/dist/app/entry.css": {
     "resourceType": "style",
