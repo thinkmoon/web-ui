9cedb776e97c16143febc2de2517efb22bdf1b78
醉月思 <tim@thinkmoon.cn>
2022-12-04 12:04:25 +0800

    chore:升级依赖

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,29 +3,28 @@ import runtimeConfig from './runtime.config';
 
 export default defineNuxtConfig({
   app: {
-    cdnURL: 'https://thinkmoon.github.io/thinkblog/',
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'keywords', content: runtimeConfig.KEYWORDS.join() },
+        { name: 'description', content: runtimeConfig.DESCRIPTION },
+      ],
+      script: [
+        {
+          src: 'https://cdn.staticfile.org/qiniu-js/3.4.1/qiniu.min.js',
+        },
+        {
+          async: true,
+          src: '/ads.js',
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
   },
-  publicRuntimeConfig: runtimeConfig,
+  runtimeConfig,
   sourcemap: {
     "server": false,
     "client": false
-  },
-  meta: {
-    meta: [
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'keywords', content: runtimeConfig.KEYWORDS.join() },
-      { name: 'description', content: runtimeConfig.DESCRIPTION },
-    ],
-    script: [
-      {
-        src: 'https://cdn.staticfile.org/qiniu-js/3.4.1/qiniu.min.js',
-      },
-      {
-        async: true,
-        src: '/ads.js',
-        crossorigin: 'anonymous',
-      },
-    ],
   },
   css: ['assets/css/index.less'],
   vite: {
