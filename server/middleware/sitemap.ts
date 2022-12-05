19acdd605f0c3d7f9068c63d3c96369eaafe2046
醉月思 <tim@thinkmoon.cn>
2022-12-05 23:03:54 +0800

    fix: 修复sitemap

--- server/middleware/sitemap.ts
+++ server/middleware/sitemap.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export default async (req, res) => {
-  if (req.url === '/sitemap.xml') {
+export default defineEventHandler(async (event) => {
+  if (event.node.req.url === '/sitemap.xml') {
     const sitemap = await axios.get('http://127.0.0.1:9447/api/sitemap.xml');
-    res.end(sitemap.data);
+    event.node.res.end(sitemap.data);
   }
-}
+});
